95b277eab99aa87115367a7ba06c253ff6e86b05
shinanhuang <122583606+shinanhuang@users.noreply.github.com>
2023-02-28 02:49:27 -0500

    Complete

---
+++ UI/End_Game.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _ready():
+	$label.text = "Thanks for playing! Your score was" + str(Global.score) + "."
+	
+
+
+func _on_Play_pressed():
+	Global.reset()
+	var _scene = get_tree().change_scene("res://Game.tscn")
+	
